b998f5fa4028aee5cdfd3e084dc134f818827267
Jonas Schmid <schmid@puzzle.ch>
2023-02-10 09:11:17 +0100

    add set 2 lab

---
+++ labs/src/main/java/ch/itninjas/labs/j7/set/lab2/MySet.java
@@ -0,0 +1,33 @@
+package ch.itninjas.labs.j7.set.lab2;
+
+public class MySet<E> implements MySetInterfaceSimple<E> {
+    @Override
+    public boolean add(E element) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object object) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object object) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
