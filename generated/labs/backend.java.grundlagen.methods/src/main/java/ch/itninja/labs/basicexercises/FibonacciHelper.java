8d395efc3673eb762cd64f46ba276da7aff6e070
andiaeg <andreas.aegerter@sbb.ch>
2026-03-22 22:54:48 +0100

    Delete generated directory

--- generated/labs/backend.java.grundlagen.methods/src/main/java/ch/itninja/labs/basicexercises/FibonacciHelper.java
+++
@@ -1,9 +0,0 @@
-package ch.itninja.labs.basicexercises;
-
-/**
- * Utility class providing helper methods for fibonacci operations.
- */
-public class FibonacciHelper {
-
-    // IT-Ninja: Füge hier Deinen Code ein...
-}
